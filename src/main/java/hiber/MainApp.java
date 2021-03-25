1ca5f4a79f78127529cd2d9eba26cbffedc359e6
Vladimir <soulexmail@gmail.com>
2021-03-25 19:55:54 +0300

    Initial commit with main program logic for review

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,50 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        Car car1 = new Car("Alfa Romeo", 1251455);
+        Car car2 = new Car("Maserati", 12514324);
+        Car car3 = new Car("Vaz 2106", 23534);
+        Car car4 = new Car("Belarus", 665544);
+
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru", car1);
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru", car2);
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru", car3);
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru", car4);
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car model = " + user.getCar().getModel());
+            System.out.println();
+        }
+
+        User user = userService.getUserByCar("Alfa Romeo", 1251455);
+        System.out.println("First Name = " + user.getFirstName());
+        System.out.println("Last Name = " + user.getLastName());
+        System.out.println("Car model = " + user.getCar().getModel());
+
+        context.close();
+    }
+}
